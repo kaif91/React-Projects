03b8ddb36ed43c0c6d9b5f1e6b5983035bb31996
Keshav Malhotra <keshav91malhotra@yahoo.in>
2022-01-17 17:57:50 +0530

    initial commit for first project

---
+++ 01-birthday-reminder/setup/src/List.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const List = ({people}) => {
+  return (
+    <>
+      { people.map((p)=> {
+        const {id,name, age, image} = p;
+        return <article key={id} className="person">
+          <img src={image} alt={name} />
+          <div>
+            <h4>{name}</h4>
+            <p>{age} years</p>
+          </div>
+        </article>
+      })}
+    </>
+  );
+};
+
+export default List;
